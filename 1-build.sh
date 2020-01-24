e280fbc6f5fca3f991df276cac0642b35b3e2fd5
Shirish Kamath <shirishk.87@gmail.com>
2020-01-25 04:09:34 +0530

    Update 1-build.sh

--- 1-build.sh
+++ 1-build.sh
@@ -1,3 +1,5 @@
+go get "gopkg.in/mcuadros/go-syslog.v2"
+
 # https://blog.codeship.com/building-minimal-docker-containers-for-go-applications/
 
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o main .
